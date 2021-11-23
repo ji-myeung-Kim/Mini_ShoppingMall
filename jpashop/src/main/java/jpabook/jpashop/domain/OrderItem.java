2d2addaec530e7a7c9138691d13d8833295f7b8f
kjm <wlaud5314@naver.com>
2021-11-23 23:39:25 +0900

    [Add] Service, Repository, Test

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.mapping.Join;
 
@@ -9,20 +11,44 @@ import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
     private int orderPrice;
     private int count;
+
+
+//    주문상품생성
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+//    주문상품취소
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+//    주문상품 총합
+    public int getTotalPrice() {
+        return getOrderPrice()*getCount();
+    }
 }
