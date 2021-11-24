60db5f2c66898bd24dc6988ff34a0a210991330d
kjm <wlaud5314@naver.com>
2021-11-24 23:08:05 +0900

    [Add&Update] FrontEnd

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.controller;
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.NotEmpty;
+@Getter @Setter
+public class MemberForm {
+    @NotEmpty(message = "이름을 작성해주세요")
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+}
