25f008a6de4c8c377aee2d086baec8bb7e11ac67
ji-myeung-Kim <wlaud5314@naver.com>
2022-01-07 22:22:00 +0900

    [엔티티 직접노출 -> DTO로 변환]

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -9,5 +11,23 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		return hibernate5Module;
+	}
+
+	/**
+	 * 이렇게 하면 lazy로 인해서 가져오지 못했던 정보들을 가져오긴하나
+	 * 직접적으로 entity 노출, 성능등을 고려했을때
+	 * 좋은 방법이 아님
+	 */
+//	@Bean
+//	Hibernate5Module hibernate5Module() {
+//		Hibernate5Module hibernate5Module = new Hibernate5Module();
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+//		return hibernate5Module;
+//	}
 
 }
